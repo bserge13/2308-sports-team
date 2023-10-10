47c3e619e1738dd97e589930d267e619f2547135
Blake Sergesketter <blakesergesketter@gmail.com>
2023-10-09 20:01:39 -0500

    Feature: team show page. Test: all testing for US 2 passes.

--- spec/features/teams/show_spec.rb
+++ spec/features/teams/show_spec.rb
@@ -1,28 +1,21 @@
 require "rails_helper"
 
 RSpec.describe "Features" do 
-  describe "#index" do 
+  describe "#show" do 
     before :each do 
       @bulls = Team.create!(team_name: "Bulls", city: "Chicago", expansion_team: true, number_of_titles: 6)
       @celtics = Team.create!(team_name: "Celtics", city: "Boston", expansion_team: false, number_of_titles: 17)
       @lakers = Team.create!(team_name: "Lakers", city: "Los Angeles", expansion_team: false, number_of_titles: 17)
       @philly = Team.create!(team_name: "76ers", city: "Philadelphia", expansion_team: true, number_of_titles: 3)
     end
-  
-    it "has an index of teams" do 
-      visit "/teams"
-
-      expect(page).to have_content(@bulls.team_name)
-      expect(page).to have_content(@bulls.city)
-
+    
+    it "has a show page with a teams attributes" do 
+      visit "/teams/#{@celtics.id}"
+      
       expect(page).to have_content(@celtics.team_name)
       expect(page).to have_content(@celtics.city)
-
-      expect(page).to have_content(@lakers.team_name)
-      expect(page).to have_content(@lakers.city)
-
-      expect(page).to have_content(@philly.team_name)
-      expect(page).to have_content(@philly.city)
+      expect(page).to have_content(@celtics.expansion_team)
+      expect(page).to have_content(@celtics.number_of_titles)
     end
   end 
 end 
