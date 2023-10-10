10438ab5fbe388c43fd0b2cb7eeaad72545d6682
Blake Sergesketter <blakesergesketter@gmail.com>
2023-10-09 19:52:59 -0500

    Feature: Team index page. All testing passes for feature testing.

---
+++ spec/features/teams/index_spec.rb
@@ -0,0 +1,29 @@
+require "rails_helper"
+
+RSpec.describe "Features" do 
+  describe "#index" do 
+    before :each do 
+      @bulls = Team.create!(team_name: "Bulls", city: "Chicago", expansion_team: true, number_of_titles: 6)
+      @celtics = Team.create!(team_name: "Celtics", city: "Boston", expansion_team: false, number_of_titles: 17)
+      @lakers = Team.create!(team_name: "Lakers", city: "Los Angeles", expansion_team: false, number_of_titles: 17)
+      @philly = Team.create!(team_name: "76ers", city: "Philadelphia", expansion_team: true, number_of_titles: 3)
+    end
+  
+    it "has an index of teams" do 
+      visit "/teams"
+
+      expect(page).to have_content(@bulls.team_name)
+      expect(page).to have_content(@bulls.city)
+
+      expect(page).to have_content(@celtics.team_name)
+      expect(page).to have_content(@celtics.city)
+
+      expect(page).to have_content(@lakers.team_name)
+      expect(page).to have_content(@lakers.city)
+
+      expect(page).to have_content(@philly.team_name)
+      expect(page).to have_content(@philly.city)
+      save_and_open_page
+    end
+  end 
+end 
